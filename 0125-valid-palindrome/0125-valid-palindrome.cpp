9273b51fe4b91b0259528c4d1fec44646efcc67b
Atika Hussain <164180561+Ruksar04@users.noreply.github.com>
2026-06-10 20:28:24 +0530

    Time: 3 ms (29.33%), Space: 10.8 MB (5.1%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string cleantext = "";
+        for(char c: s){
+            if(isalnum(c)){
+                cleantext += toupper(c);
+            }
+        }
+        int left = 0;
+        int right = cleantext.size()-1;
+        string output = cleantext;
+        while(left<right){
+            char temp = cleantext[left];
+            cleantext[left]=cleantext[right];
+            cleantext[right] = temp;
+            left++;
+            right--;
+        }
+        if(output == cleantext) return true;
+        else return false;
+    }
+};
